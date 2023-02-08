188f5157470d0bda1b767505add9117fdaab63db
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-08 12:14:40 +0300

    Задача 10. Нахождение второй цифры трехзначного числа.

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 10
+// Напишите программу, которая принимает на 
+// вход трехзначное число и на выходе 
+// показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+   
+    if (num > 99 && num < 1000)
+    {
+        int result1 = num % 100;
+        int result2 = result1 / 10;
+        System.Console.WriteLine($"{num} -> {result2}");
+    }
+
+    else
+    {
+        System.Console.WriteLine("Некорректное число");
+    }

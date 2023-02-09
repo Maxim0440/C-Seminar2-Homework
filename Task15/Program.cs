f170206c141ac4cf617f531a080657bf64fb36ec
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-09 16:27:02 +0300

    Задача15. Определение выходного дня

---
+++ Task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Напишите программу, которая принимает на вход 
+// цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+System.Console.WriteLine("Введите цифру");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+    if (num <6  && num > 0)
+    {
+        System.Console.WriteLine($"{num} -> нет");
+    }
+
+    else if (num > 5 && num <8)
+    {
+        System.Console.WriteLine($"{num} -> да");
+    }
+
+    else 
+    {
+        System.Console.WriteLine($"{num} -> нет такого дня недели");
+    }

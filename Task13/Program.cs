adad72db1fffbeedd475a2a3ef8fa7829d02accb
Maxim Kornilov <M.kornilov.flights@gmail.com>
2023-02-09 17:09:26 +0300

    Задача 13. Нахождение третьей цифры

---
+++ Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит
+//  третью цифру заданного числа или сообщает, что 
+//  третьей цифры нет.
+
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num;
+
+    while (Math.Abs(num) > 999)
+
+   {
+    num = Math.Abs(num) / 10;
+   }
+
+
+        if (Math.Abs(num) < 100)
+        {
+            System.Console.WriteLine($"{num} -> Третьей цифры нет");   
+        }
+
+        else 
+        { 
+            System.Console.WriteLine($"{result} -> {Math.Abs(num) % 10}");
+        }
+    
+      
+   
+
+   
+   
+    
+    
